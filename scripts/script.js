24b3d37ce9f73d3fb9b01c4cc9c02e2568cb8ee3
Ajmain Fayek <fayekajmain0@gmail.com>
2024-07-07 05:23:54 +0600

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+document.getElementById('hire_me').addEventListener('click', function(){
+    window.open("mailto:ajmainfayek733@gmail.com");
+});
+
+document.getElementById('resume').addEventListener('click', function(){
+    window.open("https://docs.google.com/document/d/1v2nBP5Qlgf8Y04HeoanU96GeSBtwrzAkM__H5YGbduw/edit?usp=sharing");
+});
+
+document.getElementById('github').addEventListener('click', function(){
+    window.open("https://github.com/Ajmain-Fayek");
+});
+
+document.getElementById('linkedin').addEventListener('click', function(){
+    window.open("https://www.linkedin.com/in/ajmainfayek733/");
+});
+
+document.getElementById('facebook').addEventListener('click', function(){
+    window.open("https://www.facebook.com/AjmainFayek733/");
+});
